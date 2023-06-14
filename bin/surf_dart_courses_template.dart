52fe410f88706229dcb3bf7a0a319d50caabf78d
Ilya Tsoy <inqlest@gmail.com>
2023-06-14 23:56:26 +0300

    task3

--- bin/surf_dart_courses_template.dart
+++ bin/surf_dart_courses_template.dart
@@ -1,37 +1,33 @@
-class Rifle {
-  Caliber caliber;
-  final String? _model;
-  int magazineCapacity;
+// подготовка к сессии
+void main(){
+  while( true ){
+    if(ничего не знаешь по предмету){
+      if(не готовился){
+        плакать();
+      } else {
+        посмотреть билеты();
+        написать ответы на вопросы();
+        выбрать билеты();
+      }
 
-  Rifle(this.caliber, this.magazineCapacity, this._model);
-
-  Rifle.ak47()
-      : caliber = Caliber('7.62x49', 49),
-        _model = "AK47",
-        magazineCapacity = 30;
-
-  Rifle.m1a()
-      : caliber = Caliber('7.62x51', 66),
-        _model = "M1A",
-        magazineCapacity = 10;
-
-  void shoot(var enemyHealth, int damage) {
-    if (enemyHealth <= damage) {
-      print("Enemy is dead!");
+      готовиться();
     } else {
-      print("Enemy health = ${enemyHealth - damage}");
+      continue;
     }
-  }
 
-  void reload(int magazineCapacity) {
-    print(
-        "The rigfle is reloaded with $magazineCapacity bullets in the magazine");
   }
 }
 
-class Caliber {
-  String diameter;
-  int damage;
-
-  Caliber(this.diameter, this.damage);
+void выбрать билеты() {
+  switch (уровень готовности) {
+    case вообще пусто:
+      плакать();
+      break;
+    case уверенно знаешь:
+      выучить все билеты();
+      break;
+    case немного знаешь:
+      выучить хотя бы половину();
+      break;
+  }
 }
